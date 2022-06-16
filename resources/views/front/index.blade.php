1ad40b0955cd64bedc0a18d8525449b5ac23877c
nguyentanquyen <nguyentanquyen2000@gmail.com>
2022-06-16 18:07:28 +0700

    update

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -16,8 +16,13 @@
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker.css" rel="stylesheet" type="text/css" />
 
-  <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
+    
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/js/bootstrap-datepicker.js"></script>
+
+  
     <link rel="stylesheet" href="front/style.css">
 </head>
 <body>
@@ -43,16 +48,29 @@
                                 <a href="#" class="nav-item nav-link"><i class="fa fa-solid fa-phone"></i>0123456789</a>
                             </div>
                         </div>
+                        
                     </div>
                 </nav>
+                
             </div>
+            
         </div>
+        <div class="khicau-vangdo">
+            <img src="{{Storage::disk('public')->url('khicau_vangdo.png') }}" alt="">
+        </div>  
+        
     </head>
     <main>
+        <div class="khicau-xanh">
+                <img src="{{Storage::disk('public')->url('khicau_xanh.png') }}" alt="">
+        </div>
         <div class="padding-br">
+            
             <div class="br-main">
+                
                 <div class="logo-area">
                     <div class="container">
+                    
                         <div class="row">
                             <div class="col-lg-12">
                                 <div class="logo">
@@ -63,8 +81,28 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="row">
+                        <div class="row" style="position:relative;">
+                            <div class="nguoi-toctim">
+                                <img src="{{Storage::disk('public')->url('1nguoi_tochong.png') }}" alt="">
+                            </div>
+                            <div class="khicau-do">
+                                <img src="{{Storage::disk('public')->url('khicau_do.png') }}" alt="">
+                            </div>
+                            
+                            <div class="khicau-tim">
+                                <img src="{{Storage::disk('public')->url('khicau_tim.png') }}" alt="">
+                            </div>
+                            <div class="for-nguoi">
+                                <img src="{{Storage::disk('public')->url('4nguoi.png') }}" alt="">
+                            </div>
+                            <div class="khicau-xanhnho">
+                                    <img src="{{Storage::disk('public')->url('khicau_xanhnho.png') }}" alt="">
+                            </div>
+                            <div class="khicau-tim-2">
+                                    <img src="{{Storage::disk('public')->url('khicau_tim.png') }}" alt="">
+                            </div>
                             <div class="col-lg-7 padding">
+                                
                                 <div class="border-padding">
                                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ac mollis justo. Etiam volutpat tellus quis risus volutpat, ut posuere ex facilisis.</p>
                                         <p>Suspendisse iaculis libero lobortis condimentum gravida. Aenean auctor iaculis risus, lobortis molestie lectus consequat a.</p>
@@ -93,6 +131,9 @@
                             </div>
                             
                             <div class="col-lg-4 padding">
+                                <div class="panner-ve">
+                                    <span class="img-panner">Vé của bạn</span>
+                                </div>
                                 <div class="border-padding form">
                                     <form action="#">
                                         <div class="row padding-icon">
@@ -105,12 +146,35 @@
                                         </div>
                                         <div class="row">
                                             <div class="col-lg-5">
-                                                    <input type="text" class="form-control" id="soluong" name="soluong" placeholder="Số lượng vé">
+                                                    <input type="text" class="form-control" id="soluong" name="soluong" placeholder="Số lượng vé" required>
                                                 
                                             </div>
-                                            <div class="col-lg-7">
-                                                    <input type="text" name='datepicker' class="form-control"  value="Select date" id="datepicker" ng-required="true" placeholder="MM/DD/YYYY" >
-                                                    <span class="fa fa-calendar"></span>
+                                            <div class="col-lg-7" style="position:relative;">
+                                                    
+                                                <div id="datepicker" class=" date inputdate" data-date-format="mm-dd-yyyy">
+                                                    <input class="form-control" type="text" readonly placeholder="Ngày sử dụng" required />
+                                                    <span class="input-group-addon icondate"><img src="{{Storage::disk('public')->url('calendar.png') }}" alt=""></span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-12">
+                                                <input type="text" class="form-control" id="name" name="name" placeholder="Họ và tên" required>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-12">
+                                                <input type="text" class="form-control" id="phone" name="phone" placeholder="Số điện thoại" required>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-12">
+                                                <input type="email" class="form-control" id="email" name="email" placeholder="Địa chỉ email" required>
+                                            </div>
+                                        </div>
+                                        <div class="row button">
+                                            <div class="col-lg-12">
+                                                <button type="submit">Đặt vé</button>
                                             </div>
                                         </div>
                                     </form>
@@ -129,11 +193,12 @@
             $('.js-example-basic-single').select2();
         });
 
-        $(document).ready(function() {
-  $("#datepicker").datepicker();
-  $('.fa-calendar').click(function() {
-    $("#datepicker").focus();
-  });
-});
+        $(function () {
+            $("#datepicker").datepicker({ 
+                    autoclose: true, 
+                    todayHighlight: true
+            });
+        });
+
     </script>
 </html>
